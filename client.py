3f77effc494f5ed2bc4c6e334ae6cfa53193604a
ssxrgio <scarreras10@gmail.com>
2019-05-06 22:22:52 +0200

    FINAL VERSION

--- client.py
+++ client.py
@@ -4,13 +4,17 @@ import json
 HOST = "localhost"
 PORT = 8000
 
-# The following list contains the same endpoints where verything works fine (no imput errors) as the ones on the reports, just to check/see that these endpoints do work.
+# The following list contains the same endpoints as the ones on the reports,
+# just to check/see that these endpoints do work.
 
-endpoints = ["listSpecies&json=1", "listSpecies?limit=10&json=1", "listSpecies?limit=1345&json=1", "listSpecies?limit=a&json=1",
-             "karyotype?specie=mouse&json=1", "karyotype?specie=tiger&json=1", "karyotype?specie=fish&json=1", "karyotype?specie=2&json=1",
-             "chromosomeLength?specie=mouse&chromo=18&json=1", "chromosomeLength?specie=fish&chromo=1&json=1", "chromosomeLength?specie=hello&chromo=1&json=1",
-             "chromosomeLength?specie=human&chromo=30&json=1", "chromosomeLength?specie=human&chromo=asdf&json=1", "geneSeq?gene=frat1&json=1",
-             "geneSeq?gene=frat24&json=1", "geneInfo?gene=fat1&json=1", "geneInfo?gene=frat3&json=1"]
+endpoints = ["listSpecies?json=1", "/listSpecies?limit=10&json=1", "listSpecies?limit=a&json=1", "listSpecies?limit=1234&json=1",
+             "karyotype?specie=mouse&json=1", "karyotype?specie=guinea+pig&json=1", "karyotype?specie=fish&json=1", "karyotype?specie=1%2B2&json=1",
+             "chromosomeLength?specie=mouse&chromo=18&json=1", "chromosomeLength?specie=human&chromo=33&json=1", "/chromosomeLength?specie=fish&chromo=33&json=1", "chromosomeLength?specie=guinea+pig&chromo=33&json=1",
+             "geneSeq?gene=frat1&json=1", "geneSeq?gene=fat5&json=1",
+             "geneInfo?gene=frat1&json=1", "geneInfo?gene=frat3&json=1",
+             "geneCalc?gene=frat1&json=1", "geneCalc?gene=frat3&json=1",
+             "geneList?chromo=1&start=0&end=30000&json=1", "geneList?chromo=abcd&start=12000&end=130000&json=1", "geneList?chromo=1&start=20&end=15&json=1", "geneList?chromo=1&start=1234&end=7654321&json=1", "geneList?chromo=y&start=1&end=a&json=1",
+             "geneBases&json=1"]
 
 for e in endpoints:
     conn = http.client.HTTPConnection(HOST, PORT)
@@ -18,7 +22,7 @@ for e in endpoints:
 
     r1 = conn.getresponse()
 
-    print("Response received: {}\n".format(r1.status, r1.reason))
+    print("Response received: {}".format(r1.status, r1.reason))
 
     json_data = r1.read().decode('utf-8')
 
@@ -26,4 +30,4 @@ for e in endpoints:
 
     msg = json.loads(json_data)
 
-    print("Json retrieved from {}:".format(e), msg)
+    print("Json retrieved from {}:".format(e), msg, "\n")
